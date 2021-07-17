5cbab41ee8b817ca14c6ef6b9daed39fbfcb0685
Chris Kade <chriskade1999@gmail.com>
2021-07-17 14:35:31 -0400

    Fully functional FizzBuzz

---
+++ fizzbuzz/main.js
@@ -0,0 +1,17 @@
+function runFizz() {
+    const num = document.getElementById('input').value;
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log(`FizzBuzz`);
+        } else if (i % 3 === 0) {
+            console.log(`Fizz`);
+        } else if (i % 5 === 0) {
+            console.log(`Buzz`);
+        } else {
+            console.log(i);
+        }
+    }
+    const p = document.getElementsByTagName('p');
+    p.item(0).style.display = 'block';
+}
+
