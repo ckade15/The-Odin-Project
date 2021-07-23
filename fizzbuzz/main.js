5c1af17245c5844008a5587fb2ba84d435926080
Chris Kade <chriskade1999@gmail.com>
2021-07-22 22:41:09 -0400

    Fully functional and styled number guesser

--- fizzbuzz/main.js
+++ fizzbuzz/main.js
@@ -1,17 +1,23 @@
 function runFizz() {
     const num = document.getElementById('input').value;
-    for (let i = 1; i <= num; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log(`FizzBuzz`);
-        } else if (i % 3 === 0) {
-            console.log(`Fizz`);
-        } else if (i % 5 === 0) {
-            console.log(`Buzz`);
-        } else {
-            console.log(i);
+    if (!parseInt(num)) {
+        alert('Enter a valid input');
+    } else {
+        for (let i = 1; i <= num; i++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log(`FizzBuzz`);
+            } else if (i % 3 === 0) {
+                console.log(`Fizz`);
+            } else if (i % 5 === 0) {
+                console.log(`Buzz`);
+            } else {
+                console.log(i);
+            }
         }
+        const p = document.getElementsByTagName('p');
+        p.item(0).style.display = 'block'; 
     }
-    const p = document.getElementsByTagName('p');
-    p.item(0).style.display = 'block';
+    
+    
 }
 
