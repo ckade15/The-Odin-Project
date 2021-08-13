ae5a103d0fa8d57d15119b8f8916542ffc44ecb4
Chris Kade <chriskade1999@gmail.com>
2021-08-13 12:48:54 -0400

    *

---
+++ etch-a-sketch/main.js
@@ -0,0 +1,8 @@
+
+
+let main = document.createElement('main');
+document.body.appendChild(main);
+for (let i = 0; i < 256; i++){
+    let div = document.createElement('div');
+    document.getElementById('main').appendChild(div);
+}
