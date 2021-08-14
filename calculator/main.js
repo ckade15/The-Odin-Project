1beb8ee90f87d0d11885968e4a742af900af7c5e
Chris Kade <chriskade1999@gmail.com>
2021-08-13 23:43:35 -0400

    Correctly prints value of all buttons clicked into the console

--- calculator/main.js
+++ calculator/main.js
@@ -1,1 +1,21 @@
-for (let i = 0; i <
+let feed = document.getElementById('feed');
+
+for (let i = 0; i < 18; i++){
+    const btn = document.getElementsByClassName('btn')[i].addEventListener('click', () => {
+        let p = document.createElement('p');
+        try{
+             const val = parseInt(document.getElementsByClassName('btn')[i].textContent);
+             if (val === Nan ) {
+
+             }else{
+                 console.log(val);
+                 p.textContent = 'Hi';
+                 feed.appendChild(p);
+            }
+        } 
+        catch{
+            const val = document.getElementsByClassName('btn')[i].textContent;
+            console.log(val);
+        }
+    });
+}
